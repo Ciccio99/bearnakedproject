59f365a0430f52c58186e6f6354e30473e9d61cf
Alberto Scicali <afs2842@rit.edu>
2016-05-10 12:01:46 -0400

    The current state of the project

--- bearNakedShiny.R
+++ bearNakedShiny.R
@@ -8,50 +8,65 @@
 #
 
 library(shiny)
+library(seqinr)
 
 # Define UI for application that draws a histogram
 ui <- shinyUI(fluidPage(
-   
-   # Application title
-   titlePanel("Fasta File Reader"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        # sliderInput("bins",
-        #             "Number of bins:",
-        #             min = 1,
-        #             max = 50,
-        #             value = 30),
-        fileInput("file", "Choose FASTA file",accept=c('fasta'))
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+    # Application title
+    titlePanel("Welcome to FASTAcrobat!"),
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            fileInput("file", "Choose FASTA file", accept=c('.fasta')),
+            selectInput("seqtype", 
+                        label=h5("What would you like to plot?"), 
+                        choices = list("DNA" = 1, 
+                                       "RNA" = 2, 
+                                       "Protein" = 3),
+                        selected = 3),
+            helpText("You may not select DNA or RNA if the fasta file
+                     contains a peptide sequence.")
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            h4("To get started, choose a fasta file on the left."),
+            uiOutput("fileTab"),
+            uiOutput("sum"),
+            plotOutput("distPlot")
+        )
+    )
 ))
 
 # Define server logic required to draw a histogram
 server <- shinyServer(function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-     
-     output$contents<-renderTable({
-      inFile<-input$file
-      print("hello!")
-      if(is.null(inFile)){
-        return (NULL)
-      }
-      read.csv(inFile$datapath,header=TRUE,sep=',',
-               quote=input$quote)
-      
-   })
-      
-      
-   })
+    fasta <- reactive({
+        inFile <- input$file
+        if(is.null(inFile)){
+            return ()
+        }
+        read.fasta(file = inFile$datapath)
+    })
+    output$fileTab <- renderTable({
+        if(is.null(fasta())){
+            return ()
+        }
+        input$file
+    })
+    output$sum <- renderTable({
+        if(is.null(fasta())){
+            return ()
+        }
+        summary(fasta())
+    })
+    output$distPlot <- renderPlot({
+        if(is.null(fasta())){
+            return ()
+        }
+        f <- fasta()[[1]][1:length(fasta()[[1]])]
+        barplot(table(f))
+    })
 })
 
 # Run the application 
